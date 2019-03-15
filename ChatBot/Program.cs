c753085cf185d07c04786f10ec8a3642b1d7cfc2
nanilev <dianuski99@gmail.com>
2019-03-15 11:01:40 +0000

    created alexa

---
+++ ChatBot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ChatBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string question;
+            string response;
+
+            do
+            {
+
+                Console.WriteLine("sup?");
+                question = Console.ReadLine();
+
+                switch (question)
+                {
+
+                    case "not much":
+                        response = "mood";
+                        break;
+                    case "the sky":
+                        response = "lmao";
+                        break;
+                    case "studying":
+                        response = "zzz";
+                        break;
+                    case "dying":
+                        response = "same";
+                        break;
+                    default:
+                        response = "wut?";
+                        break;
+
+
+                }
+                Console.WriteLine(response);
+
+            }
+            while (question != "exit");
+        }
+    }
+}
